eee1878e152a556405e26188c2590f94a4d43452
Logan Clements <loganclements44977@gmail.com>
2018-09-23 19:28:09 -0400

    testing react router

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import App from './App';
+import Works from './Works';
 
 const Main = () =>(
     <main>
         <Switch>
             <Route exact path = '/' component={App}/>
+            <Route path = '/works' component={Works}/>
         </Switch>
     </main>
 )
