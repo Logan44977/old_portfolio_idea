ddeafe6c54d6272c0d5bd937d76c19cdd6396ba6
Logan Clements <loganclements44977@gmail.com>
2018-09-29 19:22:27 -0400

    Added animations to the works page

--- src/works/Hero.js
+++ src/works/Hero.js
@@ -16,11 +16,22 @@ class Hero extends Component{
                     <img alt="" className="worksHero-image-lamp" src="/img/works/lamp.svg"/>
                     <img alt="" className="worksHero-image-chair" src="/img/works/chair.svg"/>
                     <img alt="" className="worksHero-image-wireframe" src="/img/works/wireframe2.svg"/>
+                    <div className="worksHero-div-wireframe"></div>
+
                     <img alt="" className="worksHero-image-menu" src="/img/works/to-do.svg"/>
+                    <div className="worksHero-div-menu"></div>
+
                     <img alt="" className="worksHero-image-arrow" src="/img/works/drawn-arrow.svg"/>
+                    <div className="worksHero-div-arrow"></div>
+
                     <img alt="" className="worksHero-image-data" src="/img/works/data.svg"/>
+                    <div className="worksHero-div-data"></div>
+
                     <img alt="" className="worksHero-image-js" src="/img/works/javascript.svg"/>
+                    <div className="worksHero-div-js"></div>
+
                     <img alt="" className="worksHero-image-android" src="/img/works/android.svg"/>
+                    <div className="worksHero-div-android"></div>
                 </div>
             </div>
        );
